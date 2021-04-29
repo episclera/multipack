f900f5128f9c7e4887c31ccc32b8a66e8d5b5a44
Gherciu Gheorghe <gherciu553@gmail.com>
2021-04-29 21:54:25 +0300

    fix: prettier breacking change

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -8,8 +8,6 @@ const eslintConfig: EslintConfig = {
     'airbnb-typescript', // extends from airbnb and override few airbnb rules to TS rules and sets ("extends": "eslint-config-airbnb" AND "parser": @typescript-eslint/parser AND "plugins": @typescript-eslint AND "import/extensions": ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.d.ts'])
     'airbnb/hooks', // sets rules for react/hooks and sets ("plugins": react-hooks AND "parserOptions.ecmaFeatures.jsx:": true)
     'plugin:prettier/recommended', // add prettier rules and sets ("extends": ["prettier"] AND "plugins": ["prettier"])
-    'prettier/react', // add prettier rules for react
-    'prettier/@typescript-eslint', // add prettier rules for TS
     'plugin:@typescript-eslint/recommended', // recommended by @typescript-eslint
     'plugin:@typescript-eslint/recommended-requiring-type-checking', // recommended by @typescript-eslint
   ],
