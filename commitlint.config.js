8d64f70d5d0ddaa774f4f3194832eddc7ba50491
Gherciu Gheorghe <gherciu553@gmail.com>
2021-09-14 16:49:27 +0300

    chore: add next public folder to ignore

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies */
 const { commitlintConfig } = require('@episclera/multipack')
 
 module.exports = commitlintConfig
